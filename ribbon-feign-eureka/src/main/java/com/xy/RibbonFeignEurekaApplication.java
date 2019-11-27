2c6c6e25d3892ac9274333dbbaf1f7e03f1c1d35
lifefucker <1373521920@qq.com>
2019-11-27 17:26:06 +0800

    add ribbon-feign

---
+++ ribbon-feign-eureka/src/main/java/com/xy/RibbonFeignEurekaApplication.java
@@ -0,0 +1,13 @@
+package com.xy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RibbonFeignEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RibbonFeignEurekaApplication.class, args);
+	}
+
+}
